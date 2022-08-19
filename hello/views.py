208b0de4d1f5df47fa8740a9192da8ada8748556
Shivams9 <91686685+Shivams9@users.noreply.github.com>
2022-08-19 13:27:06 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -77,19 +77,19 @@ def quiz(request):
     answers = request.session.get("answers")
     if answers == None:
         answers = []
-        requests.get("https://varanasisoftwarejunction.herokuapp.com/QuizQuestion/?q=dsa")
+        # requests.get("https://varanasisoftwarejunction.herokuapp.com/QuizQuestion/?q=dsa")
 
-    # q1 = {"question": "What is C?", "op1": "Language", "op2": "Alphabet", "op3": "Ascii character",
-    #       "op4": "All of these", "correct": "a"}
-    # q2 = {"question": "Who developed Python Programming language?", "op1": "Wick van rossum", "op2": "Dennis Ritchie",
-    #       "op3": "Guido van Rossum", "op4": "none", "correct": "c"}
-    # q3 = {"question": "Which of the following is the correct extension of the python file?", "op1": ".python",
-    #       "op2": ".pl", "op3": ".py", "op4": ".p", "correct": "c"}
-    # q4 = {"question": "Who developed C programming language ?", "op1": "denies ritchies", "op2": "Guido van Rossum",
-    #       "op3": "harsh", "op4": "none", "correct": "a"}
-    # q5 = {"question": "Django is  a ?", "op1": "Programming Language", "op2": "Framework",
-    #       "op3": "Python Web Framework", "op4": "None", "correct": "c"}
-    questions = []#[q1, q2, q3, q4, q5]
+    q1 = {"question": "What is C?", "op1": "Language", "op2": "Alphabet", "op3": "Ascii character",
+          "op4": "All of these", "correct": "a"}
+    q2 = {"question": "Who developed Python Programming language?", "op1": "Wick van rossum", "op2": "Dennis Ritchie",
+          "op3": "Guido van Rossum", "op4": "none", "correct": "c"}
+    q3 = {"question": "Which of the following is the correct extension of the python file?", "op1": ".python",
+          "op2": ".pl", "op3": ".py", "op4": ".p", "correct": "c"}
+    q4 = {"question": "Who developed C programming language ?", "op1": "denies ritchies", "op2": "Guido van Rossum",
+          "op3": "harsh", "op4": "none", "correct": "a"}
+    q5 = {"question": "Django is  a ?", "op1": "Programming Language", "op2": "Framework",
+          "op3": "Python Web Framework", "op4": "None", "correct": "c"}
+    questions = [q1, q2, q3, q4, q5]
     questionno = 0
     givenanswer = ""
     correctanswer = ""
